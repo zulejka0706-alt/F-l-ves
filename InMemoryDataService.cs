c1791d2e2c2601f8a7253f660a5cef5997de623a
zulejka0706-alt <zulejka0706@gmail.com>
2025-10-20 07:08:13 +0200

    Update InMemoryDataService.cs

--- InMemoryDataService.cs
+++ InMemoryDataService.cs
@@ -11,8 +11,17 @@ namespace LCNXY1_ITCompanyManager
 {
     public class InMemoryDataService : IDataService
     {
-        public ObservableCollection<Project> 
-            {}
+        public ObservableCollection<Project> LoadProjects()
+            {
+            
+var p1 =new Project()
+p1.Tasks.Add( new TaskItem(,6)
+p1.Tasks.Add( new TaskItem(,8)
+
+         var p2 =new Project()
+p2.Tasks.Add( new TaskItem(,4)
+p2.Tasks.Add( new TaskItem(,5)   
+            }
 
 
 
@@ -47,3 +56,4 @@ namespace LCNXY1_ITCompanyManager
         }
     }
 }
+
